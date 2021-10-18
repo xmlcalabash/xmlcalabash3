bf840905ec088a5dd47389d7ee87f54083e01f5d
Norman Walsh <ndw@nwalsh.com>
2021-10-18 14:32:12 +0100

    Run the test suites with EE

--- tools/test-suites.sh
+++ tools/test-suites.sh
@@ -4,5 +4,5 @@ if [ "$XPROC_TEST_SUITE" = "" ]; then
   XPROC_TEST_SUITE=/Users/ndw/Projects/xproc/test-suite/test-suite/tests
 fi
 
-sbt -Dlog4j.configurationFile=src/test/resources/log4j2.xml test \
+sbt -DsaxonEdition=EE -Dlog4j.configurationFile=src/test/resources/log4j2.xml test \
 "Test / fgRunMain com.xmlcalabash.drivers.Test -j xml-calabash.xml -c src/test/resources/config.xml $XPROC_TEST_SUITE"
