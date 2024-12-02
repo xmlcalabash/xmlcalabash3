fab496559ec5c3b907d750b440b31f2c4c0420df
Norman Walsh <ndw@nwalsh.com>
2024-12-02 17:47:42 +0000

    Renamed a few variables (code cleanup)

--- xmlcalabash/src/main/kotlin/com/xmlcalabash/runtime/XProcRuntime.kt
+++ xmlcalabash/src/main/kotlin/com/xmlcalabash/runtime/XProcRuntime.kt
@@ -5,17 +5,13 @@ import com.xmlcalabash.datamodel.DeclareStepInstruction
 import com.xmlcalabash.datamodel.PipelineVisualization
 import com.xmlcalabash.datamodel.StepDeclaration
 import com.xmlcalabash.graph.*
-import com.xmlcalabash.namespace.NsCx
 import com.xmlcalabash.namespace.NsDescription
 import com.xmlcalabash.runtime.model.CompoundStepModel
 import com.xmlcalabash.util.SaxonTreeBuilder
-import net.sf.saxon.s9api.QName
 import net.sf.saxon.s9api.XdmNode
 
 class XProcRuntime private constructor(private val start: DeclareStepInstruction) {
     companion object {
-        private val anonymous = QName(NsCx.namespace, "cx:anonymous")
-
         fun newInstance(start: DeclareStepInstruction): XProcRuntime {
             val runtime = XProcRuntime(start)
             val usedSteps = runtime.findUsedSteps(start)
@@ -64,22 +60,22 @@ class XProcRuntime private constructor(private val start: DeclareStepInstruction
     private fun initialize(start: DeclareStepInstruction, pipelines: Map<DeclareStepInstruction, SubpipelineModel>) {
         this.pipelines = pipelines
 
-        val ypipelineModels = mutableMapOf<CompoundModel, CompoundStepModel>()
+        val pipelineModels = mutableMapOf<CompoundModel, CompoundStepModel>()
 
         for ((decl, model) in pipelines) {
             val graph = model.graph
             val smodel = graph.models.filterIsInstance<SubpipelineModel>().first { it.model is PipelineModel }
 
-            val ypipelineUserStep = CompoundStepModel(this, smodel.model)  // YAtomicCompoundStep(this, smodel)
-            runnables[smodel.step.id] = ypipelineUserStep
+            val pipelineUserStep = CompoundStepModel(this, smodel.model)  // YAtomicCompoundStep(this, smodel)
+            runnables[smodel.step.id] = pipelineUserStep
             if (decl.type == start.type) {
-                pipelineStep = ypipelineUserStep
+                pipelineStep = pipelineUserStep
             }
 
-            ypipelineModels[smodel.model] = ypipelineUserStep
+            pipelineModels[smodel.model] = pipelineUserStep
         }
 
-        for ((model, step) in ypipelineModels) {
+        for ((model, step) in pipelineModels) {
             step.initialize(model)
         }
     }
