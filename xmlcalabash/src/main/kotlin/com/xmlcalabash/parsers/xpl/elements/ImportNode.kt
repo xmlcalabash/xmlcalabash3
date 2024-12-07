fea1992a9f991d88b941a59eb860e49147559b46
Norman Walsh <ndw@nwalsh.com>
2024-12-07 07:47:05 +0000

    Add special case handling for use-when on p:import

--- xmlcalabash/src/main/kotlin/com/xmlcalabash/parsers/xpl/elements/ImportNode.kt
+++ xmlcalabash/src/main/kotlin/com/xmlcalabash/parsers/xpl/elements/ImportNode.kt
@@ -5,4 +5,18 @@ import net.sf.saxon.s9api.XdmNode
 
 class ImportNode(parent: AnyNode, node: XdmNode): ElementNode(parent, node) {
     val href = node.baseURI.resolve(node.getAttributeValue(Ns.href)!!)
+    var firstPass = true
+
+    override fun computeUseWhenOnThisElement(context: UseWhenContext) {
+        super.computeUseWhenOnThisElement(context)
+        if (firstPass) {
+            firstPass = false
+        } else {
+            if (useWhen == null) {
+                useWhen = false
+                context.useWhen.remove(this)
+                context.resolvedCount++
+            }
+        }
+    }
 }
