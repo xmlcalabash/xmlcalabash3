d4ae14b89616464d2625202b1212a2c3d4b56f0a
Norman Walsh <ndw@nwalsh.com>
2025-02-22 10:02:21 +0000

    Print the underlying cause of failures, even when not in debug mode
    Running with --debug show stack traces, which are nice for debugging but
    otherwise probably a distraction. With this patch, the underlying cause of
    failures is printed even when the stack traces aren’t output. (So, for example,
    if a document is not well formed, you’ll get the underlying parser error.)

--- app/src/main/kotlin/com/xmlcalabash/app/XmlCalabashCli.kt
+++ app/src/main/kotlin/com/xmlcalabash/app/XmlCalabashCli.kt
@@ -399,6 +399,13 @@ class XmlCalabashCli private constructor() {
             if (errors[0].cause != null && errors[0].cause != errors[0]) {
                 errors[0].cause!!.printStackTrace()
             }
+        } else {
+            if (errors[0].cause != null && errors[0].cause != errors[0]) {
+                val message = errors[0].cause!!.message
+                if (message != null) {
+                    xmlCalabash.commonEnvironment.messagePrinter.println(message)
+                }
+            }
         }
 
         exitProcess(1)
